b1869d026e8317d68bd00e9781180d7908ac8cf0
Maxim Popov <maximpopov1024@gmail.com>
2019-11-06 16:44:17 +0300

    problem c, g

--- Algorithms/src/koshp/ProblemC.java
+++ Algorithms/src/koshp/ProblemC.java
@@ -1,18 +1,142 @@
 package koshp;
 
 import java.io.*;
+import java.util.*;
 
 /**
  * @author m.popov
  */
 public class ProblemC {
 
+  private static int[] parents;
+  private static LinkedList<Integer>[] components;
+  private static Map<Integer, Set<Integer>> edges;
+  private static int[] answer;
+
+  // non recursive version
+  private static int root(int x) {
+    int root = x;
+    // find root
+    while (parents[root] != root) {
+      root = parents[root];
+    }
+    // path reduction heuristic
+    int i = x;
+    while (parents[i] != i) {
+      int j = parents[i];
+      parents[i] = root;
+      i = j;
+    }
+    return root;
+  }
+
+  public static void unite(int x, int y) {
+    int rootX = root(x);
+    int rootY = root(y);
+    if (rootX == rootY) {
+      // in one set yet
+      return;
+    }
+    if (components[rootX].size() < components[rootY].size()) {
+      int t = rootY;
+      rootY = rootX;
+      rootX = t;
+    }
+    while (!components[rootY].isEmpty()) {
+      int v = components[rootY].pollFirst();
+      parents[v] = rootX;
+      components[rootX].add(v);
+    }
+  }
+
+  public static boolean equivalent(int x, int y) {
+    return root(x) == root(y);
+  }
+
+
+  private static void addRoad(int l, int r) {
+    if (equivalent(l, r)) {
+      return;
+    }
+    int rootL = root(l);
+    int rootR = root(r);
+    int maxIndex = components[rootL].size() < components[rootR].size()
+        ? rootR : rootL;
+    List<Integer> minComponent = components[rootL].size() < components[rootR].size()
+        ? components[rootL] : components[rootR];
+    for (int v : minComponent) {
+      for (int u : edges.get(v)) {
+        if (equivalent(u, maxIndex)) {
+          ++answer[v];
+          ++answer[u];
+        }
+      }
+    }
+    unite(rootL, rootR);
+  }
+
+  private static void addFriendship(int l, int r) {
+    edges.get(l).add(r);
+    edges.get(r).add(l);
+    if (equivalent(l, r)) {
+      ++answer[l];
+      ++answer[r];
+    }
+  }
+
   public static void main(String[] args) throws IOException {
     BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
     BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(System.out));
 
+    String[] parts = reader.readLine().split(" ");
+    int n = Integer.parseInt(parts[0]);
+    int m = Integer.parseInt(parts[1]);
+    int k = Integer.parseInt(parts[2]);
 
+    parents = new int[n];
+    components = new LinkedList[n];
+    edges = new HashMap<>();
+    answer = new int[n];
+    for (int i = 0; i < n; ++i) {
+      parents[i] = i;
+      components[i] = new LinkedList<>();
+      components[i].add(i);
+      edges.put(i, new HashSet<>());
+    }
 
+    for (int i = 0; i < m; ++i) {
+      parts = reader.readLine().split(" ");
+      int l = Integer.parseInt(parts[0]) - 1;
+      int r = Integer.parseInt(parts[1]) - 1;
+      addFriendship(l, r);
+    }
+
+    for (int i = 0; i < k; ++i) {
+      parts = reader.readLine().split(" ");
+      int l = Integer.parseInt(parts[0]) - 1;
+      int r = Integer.parseInt(parts[1]) - 1;
+      addRoad(l, r);
+    }
+
+    int q = Integer.parseInt(reader.readLine());
+    for (int i = 0; i < q; ++i) {
+      parts = reader.readLine().split(" ");
+      if (parts.length == 2) {
+        int v = Integer.parseInt(parts[1]) - 1;
+        writer.write(answer[v] + "\n");
+      }
+      else {
+        char t = parts[0].charAt(0);
+        int l = Integer.parseInt(parts[1]) - 1;
+        int r = Integer.parseInt(parts[2]) - 1;
+        if (t == 'T') {
+          addRoad(l ,r);
+        }
+        else {
+          addFriendship(l, r);
+        }
+      }
+    }
     writer.close();
   }
 }
--- Algorithms/src/koshp/ProblemG.java
+++ Algorithms/src/koshp/ProblemG.java
@@ -1,6 +1,8 @@
 package koshp;
 
 import java.io.*;
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * @author m.popov
@@ -11,9 +13,68 @@ public class ProblemG {
     BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
     BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(System.out));
 
-    int n = 10;
-    long m = (2 << n - 1) - 1;
-    System.out.println(m);
+    String[] parts = reader.readLine().split(" ");
+    int n = Integer.parseInt(parts[0]);
+    int m = Integer.parseInt(parts[1]);
+    int[] a = new int[n];
+    int[] sum = new int[1 << n];
+    int[] index = new int[m];
+    parts = reader.readLine().split(" ");
+    int l = -1;
+    int r = -1;
+    for (int i = 0; i < n; ++i) {
+      a[i] = Integer.parseInt(parts[i]) % m;
+      sum[1 << i] = a[i];
+      index[a[i]] = 1 << i;
+      if (a[i] == 0) {
+        l = 0;
+        r = 1 << i;
+      }
+    }
+    for (int i = 1; i < 1 << n; ++i) {
+      int prev = i & (i - 1);
+      if (prev == 0) {
+        continue;
+      }
+      sum[i] = (sum[prev] + sum[i ^ prev]) % m;
+      if (sum[i] == 0) {
+        l = 0;
+        r = i;
+        break;
+      }
+      else if (index[sum[i]] != 0) {
+        l = index[sum[i]];
+        r = i;
+        break;
+      }
+      index[sum[i]] = i;
+    }
+
+    if (l == -1 && r == -1) {
+      writer.write("" + -1);
+    }
+    else {
+      List<Integer> lIndexes = new ArrayList<>();
+      List<Integer> rIndexes = new ArrayList<>();
+      for (int i = 0; i < n; ++i) {
+        boolean lExist = (l & (1 << i)) != 0;
+        boolean rExist = (r & (1 << i)) != 0;
+        if (lExist && !rExist) {
+          lIndexes.add(i + 1);
+        }
+        if (!lExist && rExist) {
+          rIndexes.add(i + 1);
+        }
+      }
+      writer.write(lIndexes.size() + "\n");
+      for (int i = 0; i < lIndexes.size(); ++i) {
+        writer.write(lIndexes.get(i) + " ");
+      }
+      writer.write("\n" + rIndexes.size() + "\n");
+      for (int i = 0; i < rIndexes.size(); ++i) {
+        writer.write(rIndexes.get(i) + " ");
+      }
+    }
     writer.close();
   }
 }
